0dc34a169303cd94dd27f979b338ab667ee1fe19
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2018-01-21 12:51:58 +0530

    Check In pending changes

--- app/containers/Admin/index.js
+++ app/containers/Admin/index.js
@@ -1,7 +1,7 @@
-import React, { Component, PropTypes } from 'react';
-import styled from 'styled-components';
-import Helmet from 'react-helmet';
-import { connect } from 'react-redux';
+import React, { Component, PropTypes } from "react";
+import styled from "styled-components";
+import Helmet from "react-helmet";
+import { connect } from "react-redux";
 
 const AppWrapper = styled.div`
   display: flex;
@@ -10,31 +10,34 @@ const AppWrapper = styled.div`
 `;
 
 class Admin extends Component {
-	render() {
-		return (
-			<AppWrapper>
-				<Helmet
-					titleTemplate="%s - React.js Boilerplate"
-					defaultTitle="React.js Boilerplate"
-					meta={[
-						{ name: 'description', content: 'A React.js Boilerplate application' },
-					]}
-				/>
-				{this.props.children}
-			</AppWrapper>
-		);
-	}
+  render() {
+    return (
+      <AppWrapper>
+        <Helmet
+          titleTemplate="%s - React.js Boilerplate"
+          defaultTitle="React.js Boilerplate"
+          meta={[
+            {
+              name: "description",
+              content: "Yagnesh Modh Personal Application"
+            }
+          ]}
+        />
+        {this.props.children}
+      </AppWrapper>
+    );
+  }
 }
 
 Admin.propTypes = {
-	children: PropTypes.node,
-	pathname: PropTypes.string.isRequired,
+  children: PropTypes.node,
+  pathname: PropTypes.string.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-	return {
-		pathname: ownProps.location.pathname,
-	};
+  return {
+    pathname: ownProps.location.pathname
+  };
 }
 
 export default connect(mapStateToProps)(Admin);
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -6,16 +6,16 @@
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
 
-import React, { Component, PropTypes } from 'react';
-import Helmet from 'react-helmet';
-import styled from 'styled-components';
-import { connect } from 'react-redux';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import NavigationUp from 'material-ui/svg-icons/navigation/arrow-upward';
-import Header from './../../components/Header';
-import Footer from './../../components/Footer';
-import Drawer from './../../components/Drawer';
-import messages from './messages';
+import React, { Component, PropTypes } from "react";
+import Helmet from "react-helmet";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import FloatingActionButton from "material-ui/FloatingActionButton";
+import NavigationUp from "material-ui/svg-icons/navigation/arrow-upward";
+import Header from "./../../components/Header";
+import Footer from "./../../components/Footer";
+import Drawer from "./../../components/Drawer";
+import messages from "./messages";
 
 const AppWrapper = styled.div`
   display: flex;
@@ -24,11 +24,10 @@ const AppWrapper = styled.div`
 `;
 
 class App extends Component {
-
   constructor(props, context) {
     super(props, context);
     this.state = {
-      open: false,
+      open: false
     };
     this.handleToggle = this.handleToggle.bind(this);
     this.onDrawerRequestChange = this.onDrawerRequestChange.bind(this);
@@ -37,54 +36,57 @@ class App extends Component {
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener("scroll", this.handleScroll);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener("scroll", this.handleScroll);
   }
 
   onDrawerRequestChange(open) {
     this.setState({
-      open,
+      open
     });
   }
 
   handleScroll() {
     if (window.pageYOffset > 160) {
-      const header = document.getElementById('header');
-      const scrollButton = document.getElementById('scrollButton');
-      header.style.backgroundColor = 'black';
-      header.style.boxShadow = 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px';
-      scrollButton.parentElement.style.display = 'inline-block';
-    }
-    else {
-      const header = document.getElementById('header');
-      const scrollButton = document.getElementById('scrollButton');
-      header.style.backgroundColor = 'transparent';
-      header.style.boxShadow = 'none';
-      scrollButton.parentElement.style.display = 'none';
+      const header = document.getElementById("header");
+      const scrollButton = document.getElementById("scrollButton");
+      header.style.backgroundColor = "black";
+      header.style.boxShadow =
+        "rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px";
+      scrollButton.parentElement.style.display = "inline-block";
+    } else {
+      const header = document.getElementById("header");
+      const scrollButton = document.getElementById("scrollButton");
+      header.style.backgroundColor = "transparent";
+      header.style.boxShadow = "none";
+      scrollButton.parentElement.style.display = "none";
     }
   }
 
   scrollTop() {
     event.preventDefault();
-    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    window.scroll({ top: 0, left: 0, behavior: "smooth" });
   }
 
   handleToggle() {
     this.setState({
-      open: !this.state.open,
+      open: !this.state.open
     });
   }
   render() {
     return (
       <AppWrapper>
         <Helmet
-          titleTemplate="%s - React.js Boilerplate"
-          defaultTitle="React.js Boilerplate"
+          titleTemplate="%s - Yagnesh Modh"
+          defaultTitle="Yagnesh Modh"
           meta={[
-            { name: 'description', content: 'A React.js Boilerplate application' },
+            {
+              name: "description",
+              content: "Yagnesh Modh Personal Application"
+            }
           ]}
         />
         <Header
@@ -103,8 +105,15 @@ class App extends Component {
           mini={true}
           secondary={true}
           onClick={this.scrollTop}
-          style={{ position: 'fixed', bottom: 20, right: 20, display: 'none', zIndex: 2, transition: 'all 0.5s ease' }}>
-
+          style={{
+            position: "fixed",
+            bottom: 20,
+            right: 20,
+            display: "none",
+            zIndex: 2,
+            transition: "all 0.5s ease"
+          }}
+        >
           <NavigationUp />
         </FloatingActionButton>
         {React.Children.toArray(this.props.children)}
@@ -116,12 +125,12 @@ class App extends Component {
 
 App.propTypes = {
   children: PropTypes.node,
-  pathname: PropTypes.string.isRequired,
+  pathname: PropTypes.string.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
   return {
-    pathname: ownProps.location.pathname,
+    pathname: ownProps.location.pathname
   };
 }
 
